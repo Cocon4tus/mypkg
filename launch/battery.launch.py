acfe0562bd09e65021cc4924a8d645498b54f05a
k0ta s4to <obake831@gmail.com>
2025-12-31 22:31:42 +0900

    b

--- launch/battery.launch.py
+++ launch/battery.launch.py
@@ -4,18 +4,18 @@ import launch.substitutions
 import launch_ros.actions
 
 def generate_launch_description():
-    return launch.LaunchDescription([
-        # 1. 送信側（battery.py）の起動
-        launch_ros.actions.Node(
-            package='mypkg',
-            executable='battery_publisher',
-            name='pub_node'
-        ),
-        # 2. 受信側（battery_subscriber.py）の起動
-        launch_ros.actions.Node(
-            package='mypkg',
-            executable='battery_subscriber',
-            name='sub_node'
-        ),
-    ])
+    # バッテリー残量を送るノード
+    battery_node = launch_ros.actions.Node(
+        package='mypkg',
+        executable='battery',
+    )
+    # バッテリー残量を受信して表示するノード
+    reception_node = launch_ros.actions.Node(
+        package='mypkg',
+        executable='reception',
+    )
 
+    return launch.LaunchDescription([
+        battery_node,
+        reception_node
+    ])
